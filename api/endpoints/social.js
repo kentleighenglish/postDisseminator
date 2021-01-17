2fd82f2ef1d32fa5590e25dc25badeb1e6dfd367
Kentleigh English <kentleigh@ikengainnovations.com>
2021-01-17 23:42:20 +0000

    Add social api endpoint

---
+++ api/endpoints/social.js
@@ -0,0 +1,19 @@
+import * as platforms from "../../platforms";
+
+const {
+	SITE_HOST
+} = process.env;
+
+export const link = async (request, response, next) => {
+	const { platform } = request.body;
+
+	if (platform && platforms[platform] && platforms[platform].apiLink) {
+		const requestToken = await platforms[platform].apiLink({
+			callbackUrl: SITE_HOST+"/link-callback",
+			env: process.env
+		});
+
+	}
+
+	return null;
+}
